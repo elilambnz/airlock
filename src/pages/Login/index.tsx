cd0cdf06d12b7f579f16dbbc99e75739d4a35c5a
Eli Lamb <eli@dynamiccreative.co.nz>
2022-01-12 18:59:59 +1300

    Added autofocus to login inputs

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -158,6 +158,7 @@ const Login = () => {
                         name="usernameRegister"
                         type="text"
                         autoComplete="off"
+                        autoFocus
                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         onChange={(e) =>
                           setRegisterForm((prev) => ({
@@ -280,6 +281,7 @@ const Login = () => {
                       name="apiToken"
                       type="text"
                       autoComplete="off"
+                      autoFocus
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       onChange={(e) =>
                         setLoginForm((prev) => ({
